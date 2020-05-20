bb5a3c3a6e10933ea95e0b3236a467be0fcb2df2
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-05-19 18:34:42 -0700

    Add additional specifications to README.md, update scripts to replace 2 with boop! and 3 with Won't you be my neighbor? .

--- js/scripts.js
+++ js/scripts.js
@@ -6,13 +6,26 @@ let checkNumbers = function(userNumber) {
   if(userNumber < 0) {
     return wrongNumber;
   } else {
+    // create a for loop that counts from 0 to the user's number and push the number to array
     for (let i = 0; i <= userNumber; i++) {
-      numberArray.push(i);
+      // NEED TO CHANGE i TO STRING AND THEN CHECK IF STRING INCLUDES 1? - IF SO, WILL NEED TO PARSE INT i WHEN PUSHING BACK TO ARRAY
+      let numberToString = i.toString();
+      if (i === 3) {
+        numberArray.push('Won\'t you be my neighbor?');
+      } else if (i === 2) {
+        numberArray.push('Boop!');
+      } else if (i === 1) {
+        numberArray.push('Beep!');
+      } else {
+        numberArray.push(i);
+      }
     }
+
     return numberArray;
   }
 };
 
+// Need to ParseInt numbers before pushing to array
 
 
 
@@ -23,7 +36,7 @@ $(document).ready(function() {
   $('#number-form').submit(function(event) {
     event.preventDefault();
 
-    let userNumber = $('#number').val();
+    let userNumber = parseInt($('#number').val());
     let result = checkNumbers(userNumber);
     $('.number-range').text(result);
   });
