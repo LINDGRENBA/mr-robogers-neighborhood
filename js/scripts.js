dfd2772fe8c3b23d05a3eba7e10299ff23f9486f
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-05-21 10:43:46 -0700

    Refactor business logic to make code more DRY.

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,11 @@
 let checkNumbers = function(userNumber, userName) {
   let wrongNumber = 'Please enter a positive, whole number.';
   let numberArray = [];
+  let reverseArray = [];
 
   if (userNumber >= 0) {
     for (let i = 0; i <= userNumber; i++) {
-      // change i value into string 
-      let numberToString = i.toString();
-      // check if string includeds 3, 2 or 1     
+      let numberToString = i.toString(); 
       if (numberToString.includes('3')) {
         if (userName === '') {
           numberArray.push('Won\'t you be my neighbor?');
@@ -22,36 +21,14 @@ let checkNumbers = function(userNumber, userName) {
         numberArray.push(i);
       }
     }
-    return numberArray.join(', ');
+    for(let i = numberArray.length - 1; i >= 0; i--) {
+      reverseArray.push(numberArray[i]);
+    }
+    return [numberArray.join(', '), reverseArray.join(', ')];
   } else {
     return wrongNumber;
   }
 };
-
-let checkNumbersReversed = function(userNumber, userName) {
-  let reverseArray = [];
-
-    for (let i = userNumber; i >= 0; i--) {
-      // change i value into string 
-      let numberToString = i.toString();
-      // check if string includeds 3, 2 or 1     
-      if (numberToString.includes('3')) {
-        if (userName === '') {
-          reverseArray.push('Won\'t you be my neighbor?');
-        } else {
-          reverseArray.push('Won\'t you be my neighbor, ' + userName + '?' );
-        }
-      } else if (numberToString.includes('2')) {
-        reverseArray.push('Boop!');
-      } else if (numberToString.includes('1')) {
-        reverseArray.push('Beep!');
-      } else {
-        reverseArray.push(i);
-      }
-    }
-    return reverseArray.join(', ');
-}
-
 
 // USER INTERFACE LOGIC
 $(document).ready(function() {
@@ -61,16 +38,18 @@ $(document).ready(function() {
     let userNumber = parseInt($('#number').val());
     let userName = $("#name").val();
     let result = checkNumbers(userNumber, userName);
-    $('.number-range').text(result);
+    // result will have value of array [numberArray, reverseArray]
+    let resultFromZero = result[0];
+    let resultFromUserNumber = result[1];
+
+    // show range 0 to userNumber
+    $('.number-range').text(resultFromZero);
     $('.results').slideDown();
     $('.reverse-div').slideDown();
-
-    // reverse range to show from user's number to 0
+    // show range userNumber to 0
     $('.reverse-button').click(function() {
-      let reverseResult = checkNumbersReversed(userNumber, userName);
-      $('.number-range').text(reverseResult);
+      $('.number-range').text(resultFromUserNumber);
       $('.reverse-div').slideUp();
     });
   });
-
 });
